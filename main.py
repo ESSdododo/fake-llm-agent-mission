7dfd6ce940fe997b166f4c76b8be26a0daf0fc37
ESSdododo <sungjun3161@gmail.com>
2026-01-08 13:46:44 +0900

    commit test01

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ if __name__ == "__main__":
   agent = LlmAgent()
   reply = agent.handle("사용자", "안녕")
   print("응답:", reply)
+
+def get_weather (location):
+  if location == "서울":
+    return "맑음"
+  return "모름"
